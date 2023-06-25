5dd1f198fe8d0d7f37591b4e225bd599dfbd5afa
VamsiKrishna Annadanam <67264274+vamsikrishna918@users.noreply.github.com>
2023-06-25 15:38:50 +0530

    Create Ec2.tf

---
+++ Ec2.tf
@@ -0,0 +1,57 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"       #Amazon Linux ubuntu
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.TF_SG.name]
+  
+  tags = {
+    Name = "Automated_prod_server"
+  }
+}
+
+#securitygroup using Terraform
+
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-011eb6390249210aa"
+
+ 
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   ingress {
+    description      = "All traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "All"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
